3b3f371e553a43b7038395dc5ed2188dbee48e0c
kawasaki-kk <kawasaki.koki@tis.co.jp>
2016-07-01 13:50:38 +0900

    API directry 整理

--- API/APIkey_load_yaml.py
+++ API/APIkey_load_yaml.py
@@ -6,7 +6,7 @@ import sys,os
 # os.path.dirname(os.path.abspath(__file__))
 
 def load_API_KEY():
-	f = open(os.path.dirname(os.path.abspath(__file__))+u"/API_KEY.yaml","r")
+	f = open(os.path.dirname(os.path.abspath(__file__))+u"/../../API/API_KEY.yaml","r")
 	data = yaml.load(f)
 	f.close()
 	# pprint.pprint(data)
