e7baf7f5efd0a522ee45255f11d2782bf04b30c9
kawasaki-kk <kawasaki.koki@tis.co.jp>
2016-07-01 17:58:42 +0900

    geocoder added.

--- project_kerotan/kerotan/views.py
+++ project_kerotan/kerotan/views.py
@@ -1,9 +1,12 @@
+import os, sys
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from kerotan.forms import AdressForm
 
+from .gmaps_geocoder.gmaps_geocoder import get_geocode
 
 
 def display_google_map(request):
@@ -14,9 +17,11 @@ def display_google_map(request):
 		if form.is_valid():
 			print(form.cleaned_data["adress"])
 			#Get route infomation by Ekitan API.
-
-
-			return render_to_response('kerotan/test_Gmap.html', {'form':form}, RequestContext(request))
+			location = get_geocode( form.cleaned_data["adress"] )
+			if location == "get_geocode ERROR.":
+				sys.exit()
+			else:
+				return render_to_response('kerotan/test_Gmap.html', {'form':form, 'latitude':location["location"]["lat"], 'longitude':location["location"]["lng"]}, RequestContext(request))
 		else:
 			return render_to_response('kerotan/test_Gmap.html', {'form':form}, RequestContext(request))
 
@@ -24,7 +29,6 @@ def display_google_map(request):
 
 	else:
 		form = AdressForm()
-		#return HttpResponse('google map')
 		return render_to_response('kerotan/test_Gmap.html', {'form':form}, RequestContext(request))
 		# return renderrender(RequestContext(request), 'kerotan/test_Gmap.html')
 
